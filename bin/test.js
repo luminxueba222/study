401a171d74c19fde75f6c736e2dc6b93a044ae13
luminxueba222 <3037000388@qq.com>
2020-08-31 09:52:13 +0800

    数组操作

--- bin/test.js
+++ bin/test.js
@@ -45,24 +45,117 @@ function deepClone(obj) {
 // arr[1]=3
 // console.log(obj2,arr)
 
-var arr = [1, [2], [3, [4, 5]], [6, 7, [8, [9, [10, [11]]]]]]
+// var arr = [1, [2], [3, [4, 5]], [6, 7, [8, [9, [10, [11]]]]]]
 
 //法1
 // let ary = arr.flat(Infinity)
 // console.log(ary)
 
 //法2
-function _flat() {
-    let res = []
-    return  function sum(data,num) {
-        data.forEach(item => {
-            if (Array.isArray(item)) {
-                sum(item, num--)
-            } else {
-                res.push(item)
-            }
-        })
-        return res
-    }
-}
-console.log(_flat()(arr))
+// function _flat() {
+//     let res = []
+//     return  function sum(data,num) {
+//         data.forEach(item => {
+//             if (Array.isArray(item)) {
+//                 sum(item, num--)
+//             } else {
+//                 res.push(item)
+//             }
+//         })
+//         return res
+//     }
+// }
+// console.log(_flat()(arr))
+
+
+//4  递归计算 任意正整数之间的和
+// function sum(num) {
+//     if (num < 1) return 0
+//     return num+sum(--num)
+// }
+// console.log(sum(10))
+
+//5  递归计算 任意正整数之间的乘积
+
+// function sum(num) {
+//     if (num < 1) return 1
+//     return num * sum(--num)
+// }
+// console.log(sum(5))
+
+//6 递归计算 100之间能同时被3和5整除的和
+
+// function sum(num) {
+//     if (num < 15) return 0
+//     if (num % 3 === 0 && num % 5 === 0) {
+//         return num+sum(--num)
+//     }
+//     return sum(--num)
+// }
+// console.log(sum(45))
+
+//7 棋盘问题
+// let n=0
+// function sum(num) {
+//     n++
+//     if (n === num) return num*2
+//     return 2 * n +sum(num)
+// }
+// console.log(sum(3))
+
+//数组交集
+var arr1 = [1, 2, 3, 4, 5, 8, 9], arr2 = [5, 6, 7, 1,8, 9];
+// console.log(arr1.filter(item=>arr2.includes(item)))
+
+
+//数组对象交集
+// var arr1 = [{ name: 'name1', id: 1 }, { name: 'name2', id: 2 }, { name: 'name3', id: 3 }, { name: 'name5', id: 5 }];
+// arr2 = [{ name: 'name1', id: 1 }, { name: 'name2', id: 2 }, { name: 'name3', id: 3 }, { name: 'name4', id: 4 }, { name: 'name5', id: 5 }];
+    
+// console.log(arr1.filter(item=>arr2.includes(item)))
+// var arr =[]
+// arr1.forEach(item => {
+//     arr2.forEach(crt => {
+//         if (item.id === crt.id && item.name === crt.name) {
+//             arr.push(item)
+//         }
+//     })
+// })
+// console.log(arr)
+
+
+// let res = arr1.filter(item => {
+//     return arr2.some(crt => JSON.stringify(item) === JSON.stringify(crt))
+// })
+// console.log(res)
+
+
+//数组并集
+// var arr1 = [1, 2, 3, 4, 5, 8, 9], arr2 = [5, 6, 7, 1, 8, 9];
+
+var arr1 = [{ name: 'name1', id: 1 }, { name: 'name2', id: 2 }, { name: 'name3', id: 3 }, { name: 'name5', id: 5 }];
+var arr2 = [{ name: 'name1', id: 1 }, { name: 'name2', id: 2 }, { name: 'name3', id: 3 }, { name: 'name4', id: 4 }, { name: 'name5', id: 5 },{ name: 'name1', id: 1 }, { name: 'name2', id: 2 }, { name: 'name3', id: 3 }, { name: 'name5', id: 5 }];
+// 1.
+// console.log([...new Set(arr1.concat(arr2))])
+//不能去重{}类型
+//2.
+// console.log(arr1.concat(arr2.filter(item=>!arr1.includes(item))))
+
+//去重
+// var res=[]
+// arr2.forEach(item => {
+//     !res.some(crt => JSON.stringify(item) === JSON.stringify(crt))&&res.push(item)
+// })
+// console.log(res)
+
+var arr = [1, 2, 3, 4, 5, 6, 7]
+// console.log(Math.max(...arr))
+// console.log(Math.max.apply(null,arr))
+// Math.max.apply(null, arr2.map(item => item.id))
+    
+// console.log(Math.max.apply(null, arr2.map(item => item.id)))
+console.log(arr2.sort((a,b)=>b.id-a.id)[0])
+
+console.log(arr1.reduce((prve, cur) => {
+    return prve + cur.id
+},0))
